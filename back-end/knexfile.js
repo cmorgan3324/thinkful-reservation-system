4481f8a0d58266ce779601622fbc328d9894f080
Cory Morgan <132379707+cmorgan3324@users.noreply.github.com>
2023-12-21 13:15:25 -0500

    Update knexfile.js

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -2,20 +2,18 @@
  * Knex configuration file.
  *
  * You will not need to make changes to this file.
- * DATABASE_URL_DEVELOPMENT = "postgres://gjnhmrob:D3GN88IgioUsDiiguwPjBScGpGCP5i8P@berry.db.elephantsql.com/gjnhmrob",
- * DATABASE_URL_TEST = "postgres://brxgzbsn:QhGBeFgYjJHFwihc0_2bB2WsP0kVTz2f@berry.db.elephantsql.com/brxgzbsn",
- * DATABASE_URL_PREVIEW = "postgres://pmkrpktu:Bc19jFQSpoP2lyApZGNmtz5KJy1Yn7qT@berry.db.elephantsql.com/pmkrpktu",
  */
 
 require('dotenv').config();
 const path = require("path");
 
-
-
 const {
-DATABASE_URL = "postgres://tyhpxxtt:kppwFEbqBW66mdoiwZNI_uwmUgB9OwvP@berry.db.elephantsql.com/tyhpxxtt",
-DEBUG,
-} = process.env;
+  DATABASE_URL = "postgres://tyhpxxtt:kppwFEbqBW66mdoiwZNI_uwmUgB9OwvP@berry.db.elephantsql.com/tyhpxxtt",
+  DATABASE_URL_DEVELOPMENT = "postgres://gjnhmrob:D3GN88IgioUsDiiguwPjBScGpGCP5i8P@berry.db.elephantsql.com/gjnhmrob",
+  DATABASE_URL_TEST = "postgres://brxgzbsn:QhGBeFgYjJHFwihc0_2bB2WsP0kVTz2f@berry.db.elephantsql.com/brxgzbsn",
+  DATABASE_URL_PREVIEW = "postgres://pmkrpktu:Bc19jFQSpoP2lyApZGNmtz5KJy1Yn7qT@berry.db.elephantsql.com/pmkrpktu",
+  DEBUG,
+  } = process.env;
 
 module.exports = {
   development: {
