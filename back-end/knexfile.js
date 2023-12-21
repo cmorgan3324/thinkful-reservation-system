1d3751e70e967159fd37888f58b27d5d61bea17d
Cory Morgan <132379707+cmorgan3324@users.noreply.github.com>
2023-12-21 13:29:32 -0500

    Update knexfile.js to seed production database

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -8,12 +8,18 @@ require('dotenv').config();
 const path = require("path");
 
 const {
+  NODE_ENV = "production",
   DATABASE_URL = "postgres://tyhpxxtt:kppwFEbqBW66mdoiwZNI_uwmUgB9OwvP@berry.db.elephantsql.com/tyhpxxtt",
   DATABASE_URL_DEVELOPMENT = "postgres://gjnhmrob:D3GN88IgioUsDiiguwPjBScGpGCP5i8P@berry.db.elephantsql.com/gjnhmrob",
   DATABASE_URL_TEST = "postgres://brxgzbsn:QhGBeFgYjJHFwihc0_2bB2WsP0kVTz2f@berry.db.elephantsql.com/brxgzbsn",
   DATABASE_URL_PREVIEW = "postgres://pmkrpktu:Bc19jFQSpoP2lyApZGNmtz5KJy1Yn7qT@berry.db.elephantsql.com/pmkrpktu",
   DEBUG,
   } = process.env;
+
+  const URL =
+  NODE_ENV === "production"
+    ? DATABASE_URL
+    : DATABASE_URL_DEVELOPMENT;
 
 module.exports = {
   development: {
