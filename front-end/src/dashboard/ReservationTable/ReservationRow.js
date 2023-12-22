91b79bb6f19d5f530e4adfafb5a6119f1b68d27f
Cory Morgan <132379707+cmorgan3324@users.noreply.github.com>
2023-12-21 21:28:50 -0500

    Update ReservationRow.js

--- front-end/src/dashboard/ReservationTable/ReservationRow.js
+++ front-end/src/dashboard/ReservationTable/ReservationRow.js
@@ -42,13 +42,13 @@ export default function ReservationRow({ reservation, cancelRes }) {
         </Link>
       </td>
       <td>
-        <Link
+        <button
           className="btn btn-danger"
           data-reservation-id-cancel={reservation.reservation_id}
           onClick={handleCancel}
         >
           Cancel
-        </Link>
+        </button>
       </td>
     </tr>
   );
